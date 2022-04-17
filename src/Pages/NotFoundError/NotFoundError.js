cee5b36ea508197b1a1bfd51ef6d3f2dd18c849a
muhayminul <muhayminul96@gmail.com>
2022-04-17 15:31:17 +0600

    not found page added

---
+++ src/Pages/NotFoundError/NotFoundError.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundError = () => {
+  return (
+    <div>
+      <img className='w-100' src="https://i.ibb.co/qkhVgJ8/funny-404-page.jpg" alt="" />
+    </div>
+  );
+};
+
+export default NotFoundError;
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2>  &copy; Cholo Ghuri {new Date().getFullYear()} </h2>
+        </div>
+    );
+};
+
+export default Footer;
